161839f0af6e0d935d88d97ce4003236985702b5
Werlesson <werlessono@gmail.com>
2019-05-23 11:30:41 -0300

    Implementaçao do Sort e Query

--- src/controllers/cid10-controller.js
+++ src/controllers/cid10-controller.js
@@ -1,16 +1,26 @@
 "use strict";
 const mongoose = require("mongoose");
 const Cid10 = mongoose.model("Cid10");
+const reg = require("../mixin/regexIgnoreAccents");
 
 exports.get = (req, res, next) => {
-  const { page, perPage, subcat } = req.query;
+  const { page, perPage, subcat, search } = req.query;
+
+  const query = {
+    $or: [
+      { SUBCAT: new RegExp(reg.make_pattern(search), "i") },
+      { DESCRICAO: new RegExp(reg.make_pattern(search), "i") }
+    ]
+  };
+
   const options = {
     page: parseInt(page, 10) || 1,
     limit: parseInt(perPage, 10) || 10,
-    sort: { SUBCAT: subcat || "asc" },
+    sort: { SUBCAT: subcat || "desc" },
     select: "SUBCAT DESCRICAO DESCRABREV"
   };
-  Cid10.paginate({}, options)
+
+  Cid10.paginate(query, options)
     .then(data => {
       res.status(200).send(data);
     })
